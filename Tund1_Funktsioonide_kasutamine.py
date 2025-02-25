27689dd89618f430fe72f91d6b306af48d41b377
Eldar Aliev <eldar040503@gmail.com>
2025-02-25 15:47:58 +0200

    #

---
+++ Tund1_Funktsioonide_kasutamine.py
@@ -0,0 +1,32 @@
+from math import *
+from random import * #importeerimine kõik funktsioonid
+#Ülesanne1
+# nimi=input("Mis on sinu nimi?: ")
+# vanus=int(input("Kui vana sa oled?: "))
+
+# print(f"Tere, maailm! Tervitan sind {nimi} Sa oled {vanus} aastat vana.")
+# print("Tere, maailm! Tervitan sind" , nimi, "Sa oled", vanus, "aastat vana.")
+# print("Tere, maailm! Tervitan sind"+nimi+ "\nSa oled"+str(vanus)+"aastat vana.")
+# #Ülesanne2
+# vanus = 18
+# eesnimi = "Jaak"
+# pikkus = 16.5
+# kas_käib_koolis = True
+# print(f"Muutuja {vanus} on {type(vanus)} tüübi")
+# print(f"Muutuja {eesnimi} on {type(eesnimi)} tüübi")
+# print(f"Muutuja {pikkus} on {type(pikkus)} tüübi")
+# print(f"Muutuja {kas_käib_koolis} on {type(kas_käib_koolis)} tüübi")
+
+# #Ülesanne3
+# kommidearv=randint(1,100)
+# print(f"Laual on {kommidearv} kommi.")
+# kommidvõtnud=int(input("Mitu kommi tahad ära võtta?: "))
+# onjäänud=kommidearv-kommidvõtnud
+# print(f"Laual on jäänud {onjäänud} komme.")
+
+#ulesanne4
+ümbermõõdu=int(input("Sisestage puu ümbermõõdu?: "))
+läbimõõdu=ümbermõõdu/pi
+print(f"Puu läbimõõdu on {läbimõõdu} .")
+
+#ülesanne5
