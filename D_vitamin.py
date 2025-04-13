406b1b03829efe0638b9387a000b56fef57e9701
funky <funky@CHAPPA>
2025-04-13 22:34:02 +0300

    ..

--- D_vitamin.py
+++ D_vitamin.py
@@ -1,11 +1,11 @@
 from D_vitamin_module import *
 
-nimed, d_vitamiin = sisesta_andmed() # Заполнение данных
+nimed, d_vitamiin = sisesta_andmed() #Заполнение данных
 
 while True:
     print("\nAndmed:")
-    loend = zip(nimed, d_vitamiin)  # Объединяем имена и уровни витамина
-    for nimi, d_tase in loend:  # Проходимся по каждому пациенту
+    loend = zip(nimed, d_vitamiin)  #Объединяем имена и уровни витамина
+    for nimi, d_tase in loend:  #Проходимся по каждому пациенту
         print(f"{nimi}: {d_tase} mcg/l")
     print("\nMenu:")
     print("1 - D-vitamiini vaegusega patsiendid (alla 30)\n2 - Leia ja kuva keskmine D-vitamiini tase\n3 - Näita K patsienti kõige kõrgema D-vitamiini tasemega\n4 - Otsi patsiendi nime järgi ja kuva tema D-vitamiini tase\n5 - Kuva patsiendid, kelle D-vitamiini tase on alla keskmise\n0 - Välja")
