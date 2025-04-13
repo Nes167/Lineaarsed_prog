ca9c1a5a3b4b59f949dd0307b258469200bb4b7b
funky <funky@CHAPPA>
2025-04-13 22:33:24 +0300

    ...

--- D_vitamin.py
+++ D_vitamin.py
@@ -1,18 +1,26 @@
-from D_vitamin import *
+from D_vitamin_module import *
 
-nimed=[]
-D_vitamiini_sisaldus=[] 
-def patsiendid():
-    nimed,d_vitamiin=sisesta_andmed()
+nimed, d_vitamiin = sisesta_andmed() # Заполнение данных
 
 while True:
-    print("Andmed: ")
-    print(nimed)
-    print(D_vitamiini_sisaldus)
-    print("Menu:")
-    print("1-Leia keskmine D-vitamiini tase\n2-Kuvage K kõrgeima punktisumma saanud inimese nimekiri\n3-Otsige töötajaid nime järgi ja kuvage tulemused ekraanile\n4-oma variant")
-    valik=int(input())
+    print("\nAndmed:")
+    loend = zip(nimed, d_vitamiin)  # Объединяем имена и уровни витамина
+    for nimi, d_tase in loend:  # Проходимся по каждому пациенту
+        print(f"{nimi}: {d_tase} mcg/l")
+    print("\nMenu:")
+    print("1 - D-vitamiini vaegusega patsiendid (alla 30)\n2 - Leia ja kuva keskmine D-vitamiini tase\n3 - Näita K patsienti kõige kõrgema D-vitamiini tasemega\n4 - Otsi patsiendi nime järgi ja kuva tema D-vitamiini tase\n5 - Kuva patsiendid, kelle D-vitamiini tase on alla keskmise\n0 - Välja")
+    valik = input("\nSisesta valik: ")
     if valik == "1":
-            puudus_vitamiin_d(nimed, d_vitamiin)
+        puudus_vitamiin_d(nimed, d_vitamiin)
     elif valik == "2":
         keskmine_vitamiin_d(d_vitamiin)
+    elif valik == "3":
+        top_k_vitamiin_d(nimed, d_vitamiin)
+    elif valik == "4":
+        otsi_patsiendi_vitamiin_d(nimed, d_vitamiin)
+    elif valik == "5":
+        alla_keskmise(nimed, d_vitamiin)
+    elif valik == "0":
+        break
+    else:
+        print("Viga! Proovi veel kord.")
--- D_vitamin_module.py
+++ D_vitamin_module.py
@@ -1,28 +1,95 @@
 from random import *
 
-def sisesta_andmed ():
-    """Добавляем пациентов в список
+def sisesta_andmed():
+    """Ввод данных. имена пациентов и генерация Д-витамина
+    :return: Списки с именами и значениями витамина Д
     """
-    nimed=[]
-    D_vitamiini_sisaldus=[]
+    nimed = []  #Список для имён пациентов
+    d_vitamiin = []  #Список для значений витамина D
+
     try:
-        n=int(input("Kui palju patsiente analüüsida? ")).isalpha()
-        for i in range(n):
-            nimi=input("Sisesta nimi: ")
-            nimed.append(nimi)
-            d_vitamiin=random(10,100)
-            D_vitamiini_sisaldus.append(d_vit)
-    except: 
-        print("Sisesta ainult tähed")
-    return nimed, d_vitamiin
+        kogus = int(input("Mitu patsienti soovid sisestada? "))  #Сколько пациентов ввести?
+        if kogus < 0:
+            print("Palun sisesta positiivne arv.")  #Сообщение, если число не положительное
+            return [], []
+        for i in range(kogus):#Вводим имена по одному
+            nimi = input(f"Sisesta patsiendi nr {i+1} nimi: ").capitalize()  #Ввод имени
+            nimed.append(nimi)  #Добавляем имя в список
+            d_tase = randint(10, 80)  #Рандом уровень Д-витамина от 10 до 80
+            d_vitamiin.append(d_tase) #Добавляем значение в список
+        return nimed, d_vitamiin  #Возвращаем списки
+    except:
+        print("Viga! palun sisesta täisarv.")  #Сообщение об ошибке, если введено не число
+        return [], []
+
 
 def puudus_vitamiin_d(nimed, d_vitamiin):
-    print("\nD-vitamiini vaegusega patsiendid (alla 30):")
-    for nimi, d_sisaldus in zip(nimed, d_vitamiin):
-        if d_sisaldus < 30:
-            print(f"{nimi}: {d_sisaldus} mcg/l")
+    """ Функция для отображения пациентов с недостатком витамина Д (<30)
+    :param list nimed: Список имён пациентов
+    :param list d_vitamiin: Список значений витамина Д
+    """
+    print("Patsiendid, kellel on D-vitamiini puudus (<30):")
+    leitud = False
+    for i in range(len(nimed)):
+        if d_vitamiin[i] < 30:  #Проверяем уровень
+            print(f"{nimed[i]} : {d_vitamiin[i]} mcg/l")  #Выводим результат у кого недостаток Д витамина
+            leitud = True
+    if not leitud:
+        print("Kõik patsiendid on normaalsel tasemel.")  #Если ни у кого нет дефицита
+
 
 def keskmine_vitamiin_d(d_vitamiin):
-    average_vitamiin = sum(d_vitamiin) / len(d_vitamiin)
-    print(f"\nKeskmine D-vitamiini tase: {average_vitamiin} mcg/l")
+    """Функция для расчёта и вывода среднего значения витамина Д
+    :param list d_vitamiin: Список значений витамина Д
+    """
+    summa = sum(d_vitamiin) #Складываем все значения
+    keskmine = summa / len(d_vitamiin) #Делим на количество
+    print("Keskmine D-vitamiini tase:", round(keskmine, 2), "mcg/l") #Выводим результат
+
+
+def top_k_vitamiin_d(nimed, d_vitamiin):
+    """Топ K пациентов по уровню витамина Д
+    :param list nimed: Список имён пациентов
+    :param list d_vitamiin: Список значений витамина Д
+    """
+    try:
+        k = int(input("Mitu parimat patsienti soovid näha? "))  #Сколько лучших хотим увидеть
+        if k <= 0:
+            print("Palun sisesta positiivne arv.")
+            return
+        top = sorted(zip(nimed, d_vitamiin), reverse=True)[:k] #Объединяем имена и значения. Сортируем в порядке убывания и берём столько имён сколько ввёл пользователь
+        print("Parimad patsiendid:")
+        for nimi, d_tase in top:
+            print(f"{nimi} : {d_tase}")
+    except:
+        print("Palun sisestage korrektne arv!")
+
+ 
+def otsi_patsiendi_vitamiin_d(nimed, d_vitamiin):
+    """Поиск пациента по имени и вывода его уровня Д
+    :param list nimed: Список имён пациентов
+    :param list d_vitamiin: Список значений витамина Д
+    """
+    nimi = input("Sisesta patsiendi nimi, keda otsid: ").capitalize()  #Запрашиваем имя для поиска
+    leitud = False
+    for i in range(len(nimed)): #Перебираем список и ищем имя, которое ввёл пользователь
+        if nimed[i]==nimi:
+            print(f"{nimed[i]} : {d_vitamiin[i]} mcg/l")
+            leitud = True
+    if not leitud:
+        print("Sellist patsienti ei leitud.")  # Если не найдено
+
+def alla_keskmise(nimed, d_vitamiin):
+    """Показывает пациентов у которых уровень Двитамина ниже среднего.
+    :param list nimed: Список имён пациентов
+    :param list d_vitamiin: Список значений витамина Д
+    """
+    summa = sum(d_vitamiin) 
+    keskmine = summa / len(d_vitamiin) #Считаем среднее значение витамина Д
+    print(f"Keskmine D-vitamiini tase: {round(keskmine, 2)} mcg/l")
+    print("Patsiendid alla keskmise:")
+    alla=zip(nimed, d_vitamiin)
+    for nimi, d_tase in alla: #Ищем пациентов c значением ниже среднего
+        if d_tase < keskmine:
+            print(f"{nimi}: {d_tase} mcg/l")
 
