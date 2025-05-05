811515d4a9b74f4ec5e12edea6f86fed512936f4
funky <funky@CHAPPA>
2025-05-05 19:54:33 +0300

    ....

--- teema8_matplotlib.py
+++ teema8_matplotlib.py
@@ -53,72 +53,72 @@ plt.show()
 
 #babochka
 
-# x=np.arange(-9,-1,1)#y
-# y=-1/8*(x+9)**2+8
+x=np.linspace(-9, -1, 100)#y
+y=-1/8*(x+9)**2+8
 
-# x1=np.arange(1,9,1)#b
-# y1=-1/8*(x1-9)**2+8
+x1=np.linspace(1, 9, 100)#b
+y1=-1/8*(x1-9)**2+8
 
-# x2=np.linspace(-9,-8,15)#y
-# y2=7*((x2+8)**2)+1
+x2=np.linspace(-9,-8,100)#y
+y2=7*(x2+8)**2+1
 
-# x3=np.linspace(8,9,15)#b
-# y3=7*(x3-8)**2+1
+x3=np.linspace(8,9,100)#b
+y3=7*(x3-8)**2+1
 
-# x4=np.arange(-8,-1,1)#y
-# y4=1/49*(x4+1)**2
+x4=np.linspace(-8,-1,100)#y
+y4=1/49*(x4+1)**2
 
-# x5=np.arange(1,8,1)#b
-# y5=1/49*(x5-1)**2
+x5=np.linspace(1,8,100)#b
+y5=1/49*(x5-1)**2
 
-# x6=np.arange(-8,-1,1)#l
-# y6=(-4/49)*(x6+1)**2
+x6=np.linspace(-8,-1,100)#l
+y6=(-4/49)*(x6+1)**2
 
-# x7=np.arange(1,8,1)#p
-# y7=(-4/49)*(x7-1)**2
+x7=np.linspace(1,8,100)#p
+y7=(-4/49)*(x7-1)**2
 
-# x8=np.arange(-8,-2,1)#l
-# y8=1/3*(x8+5)**2-7
+x8=np.linspace(-8,-2,100)#l
+y8=1/3*(x8+5)**2-7
 
-# x9=np.arange(2,8,1)#p
-# y9=1/3*(x9-5)**2-7
+x9=np.linspace(2,8,100)#p
+y9=1/3*(x9-5)**2-7
 
-# x10=np.linspace(-2,-1,10)#l
-# y10=-2*(x10+1)**2-2
+x10=np.linspace(-2,-1,100)#l
+y10=-2*(x10+1)**2-2
 
-# x11=np.linspace(1,2,10)#p
-# y11=-2*(x11-1)**2-2
+x11=np.linspace(1,2,100)#p
+y11=-2*(x11-1)**2-2
 
-# x12=np.linspace(-1,1,15)
-# y12=-4*x12**2+2
+x12=np.linspace(-1,1,100)
+y12=-4*x12**2+2
 
-# x13=np.linspace(1,-1,15)
-# y13=4*x13**2-6
+x13=np.linspace(1,-1,100)
+y13=4*x13**2-6
 
-# x14=np.linspace(-2,0,10)
-# y14=(-1.5)*x14+2
+x14=np.linspace(-2,0,100)
+y14=(-1.5)*x14+2
 
-# x15=np.linspace(0,2,10)
-# y15=1.5*x15+2
+x15=np.linspace(0,2,100)
+y15=1.5*x15+2
 
-# plt.plot(x, y, linestyle='-', marker='o', color='yellow')
-# plt.plot(x1, y1, linestyle='-', marker='o', color='blue')
-# plt.plot(x2, y2, linestyle='-', marker='o', color='yellow')
-# plt.plot(x3, y3, linestyle='-', marker='o', color='blue')
-# plt.plot(x4, y4, linestyle='-', marker='o', color='yellow')
-# plt.plot(x5, y5, linestyle='-', marker='o', color='blue')
-# plt.plot(x6, y6, linestyle='-', marker='o', color='lightblue')
-# plt.plot(x7, y7, linestyle='-', marker='o', color='pink')
-# plt.plot(x8, y8, linestyle='-', marker='o', color='lightblue')
-# plt.plot(x9, y9, linestyle='-', marker='o', color='pink')
-# plt.plot(x10, y10, linestyle='-', marker='o', color='lightblue')
-# plt.plot(x11, y11, linestyle='-', marker='o', color='pink')
-# plt.plot(x12, y12, linestyle='-', marker='o', color='red')
-# plt.plot(x13, y13, linestyle='-', marker='o', color='red')
-# plt.plot(x14, y14, linestyle='-', marker='o', color='red')
-# plt.plot(x15, y15, linestyle='-', marker='o', color='red')
-# plt.grid()
-# plt.show()
+plt.plot(x, y, linestyle='-', marker='o', color='yellow')
+plt.plot(x1, y1, linestyle='-', marker='o', color='blue')
+plt.plot(x2, y2, linestyle='-', marker='o', color='yellow')
+plt.plot(x3, y3, linestyle='-', marker='o', color='blue')
+plt.plot(x4, y4, linestyle='-', marker='o', color='yellow')
+plt.plot(x5, y5, linestyle='-', marker='o', color='blue')
+plt.plot(x6, y6, linestyle='-', marker='o', color='lightblue')
+plt.plot(x7, y7, linestyle='-', marker='o', color='pink')
+plt.plot(x8, y8, linestyle='-', marker='o', color='lightblue')
+plt.plot(x9, y9, linestyle='-', marker='o', color='pink')
+plt.plot(x10, y10, linestyle='-', marker='o', color='lightblue')
+plt.plot(x11, y11, linestyle='-', marker='o', color='pink')
+plt.plot(x12, y12, linestyle='-', marker='o', color='red')
+plt.plot(x13, y13, linestyle='-', marker='o', color='red')
+plt.plot(x14, y14, linestyle='-', marker='o', color='red')
+plt.plot(x15, y15, linestyle='-', marker='o', color='red')
+plt.grid()
+plt.show()
 
 
 # x = [1, 2, 3, 4, 5]
