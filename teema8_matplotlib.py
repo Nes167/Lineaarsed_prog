03fe43f5b198de895a1a0be74f2d1022cdb7c50f
DariaHalchenko <daragalcenko3@gmail.com>
2025-05-06 14:32:10 +0300

    ...

--- teema8_matplotlib.py
+++ teema8_matplotlib.py
@@ -40,16 +40,16 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
-# x=np.arange(-10,10,1)# -10,-9,-8....10  linspace(-10,10,100)-punktidearv
-# y=2*x**2-4*x+5
-# plt.figure(facecolor='yellow')
-# plt.plot(x, y)
-# plt.xlabel("x")
-# plt.ylabel("2*x**2-4*x+5")
-# plt.title("Graafik")
-# plt.grid()
-# plt.savefig('graafik.png')
-# plt.show()
+x=np.arange(-10,10,1)# -10,-9,-8....10  linspace(-10,10,100)-punktidearv
+y=2*x**2-4*x+5
+plt.figure(facecolor='yellow')
+plt.plot(x, y)
+plt.xlabel("x")
+plt.ylabel("2*x**2-4*x+5")
+plt.title("Graafik")
+plt.grid()
+plt.savefig('graafik.png')
+plt.show()
 
 #babochka
 
@@ -118,30 +118,30 @@ plt.plot(x13, y13, linestyle='-', marker='o', color='red')
 plt.plot(x14, y14, linestyle='-', marker='o', color='red')
 plt.plot(x15, y15, linestyle='-', marker='o', color='red')
 plt.grid()
+plt.savefig('babochka.png')
 plt.show()
 
 
-# x = [1, 2, 3, 4, 5]
-# y = [1, 4, 9, 16, 25]
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
 
-# plt.plot(x, y, linestyle='--', marker='o', color='red',markersize=8,  markeredgecolor='yellow', markerfacecolor='lightblue')
-# plt.title("Lihtne graafik")
-# plt.xlabel("x telg")
-# plt.ylabel("y telg")
-# plt.show()
+plt.plot(x, y, linestyle='--', marker='o', color='red',markersize=8,  markeredgecolor='yellow', markerfacecolor='lightblue')
+plt.title("Lihtne graafik")
+plt.xlabel("x telg")
+plt.ylabel("y telg")
+plt.show()
 
-# plt.scatter(x, y, color='lightblue', label='Punktidest diagramm')
-# plt.legend()
-# plt.show()
+plt.scatter(x, y, color='lightblue', label='Punktidest diagramm')
+plt.legend()
+plt.show()
 
-# plt.bar(x, y, color='pink', label='Tulpdiagramm')
-# plt.legend()
-# plt.show()
+plt.bar(x, y, color='pink', label='Tulpdiagramm')
+plt.legend()
+plt.show()
 
-# plt.hist(x, y, color='red', label='Histogramm')
-# plt.legend()
-# plt.show()
+plt.hist(x, y, color='red', label='Histogramm')
+plt.legend()
+plt.show()
 
-# plt.pie(x, y, colors=['gray','yellow','red','blue','pink']
-# plt.legend()
+# plt.pie(x, y, color='red')
 # plt.show()
