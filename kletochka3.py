311a8e5ef55a5d45a1731f92be16bb1fc99d7cc0
DariaHalchenko <daragalcenko3@gmail.com>
2025-05-23 15:28:37 +0300

    .....

--- kletochka3.py
+++ kletochka3.py
@@ -10,7 +10,7 @@ pind=pygame.display.set_mode([ekraan_w,ekraan_h])
 pygame.display.set_caption("Ruuduvõrk")
 pind.fill(valge)
 
-def ruudustik(pind,ruudu_w,ruudu_h, read, veerud):
+def ruudustik(pind,ruudu_w,ruudu_h, veerud, read):
     for rida in range(read):
         for veerg in range(veerud):
             varv=[random.randint(0,255),random.randint(0,255),random.randint(0,255)]
@@ -22,7 +22,7 @@ ruudu_h=random.randint(10,50)
 read=ekraan_w//ruudu_w+1
 veerud=ekraan_h//ruudu_h+1
 
-ruudustik(pind,ruudu_h,ruudu_w,read,veerud)
+ruudustik(pind,ruudu_w,ruudu_h,read,veerud)
 
 pygame.display.flip()
 
