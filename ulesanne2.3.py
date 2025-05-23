9907fa62ab53a23c3b204b69d0b0992f08023e02
DariaHalchenko <daragalcenko3@gmail.com>
2025-05-23 13:43:01 +0300

    ...

--- ulesanne2.3.py
+++ ulesanne2.3.py
@@ -8,10 +8,17 @@ green = [0, 255, 0]
 blue = [0, 0, 255]
 pink = [255, 153, 255]
 lGreen = [153, 255, 153]
+ekraan_w=640
+ekraan_h=480
 
-pind=pygame.display.set_mode([640,480])
+pind=pygame.display.set_mode([ekraan_w,ekraan_h])
 pygame.display.set_caption("Majake")
 pind.fill(lGreen)
+
+maja_w=ekraan_w *(1/3)
+maja_h=ekraan_h * (1/2)
+x=(ekraan_w - maja_w) /2
+y=(ekraan_h + maja_h * (3/4)) /2 
 
 #functions
 def drawHouse(x, y, width, height, screen, color):
@@ -20,18 +27,35 @@ def drawHouse(x, y, width, height, screen, color):
     pygame.draw.lines(screen, color, False, points, lineThickness)
 
 def drawDoor(x, y, width, height, screen, color):
-    door_width=width/4
-    door_height=height/2
-    door_x= x+(width-door_width)/2
-    door_y= y-door_height
-    points = [(door_x,y), (door_x,door_y), (door_x+door_width,door_y), (door_x+door_width, y),(door_x,y)]
+    door_w=width/4
+    door_h=height/2
+    door_x= x+(width-door_w)/2
+    door_y= y-door_h
+    points = [(door_x,y), (door_x,door_y), (door_x+door_w,door_y), (door_x+door_w, y),(door_x,y)]
     lineThickness = 3
     pygame.draw.lines(screen, color, False, points, lineThickness)
 
-drawHouse(100,400,300,400,pind,red)
-drawDoor(100, 400, 300, 400, pind, blue)
-# drawWindow(100, 400, 300, 400, pind, pink)
-# drawChimney(100, 400, 300, 400, pind, red)
+def drawWindow(x, y, width, height, screen, color):
+    window_size = width / 5
+    window_x=x+width *(1/6)
+    window_y=y-height *(1/2)
+    points=[(window_x, window_y),(window_x + window_size, window_y),(window_x + window_size, window_y + window_size),(window_x, window_y + window_size),(window_x, window_y)]
+    lineThickness = 3
+    pygame.draw.lines(screen, True, color, points, lineThickness)
+
+def drawChimney(x, y, width, height, screen, color):
+    chimney_w = width/10
+    chimney_h = height/4
+    chimney_x = x + width*0.7
+    chimney_y =y-height-chimney_h+20
+    points = [(chimney_x, chimney_y + chimney_h),(chimney_x, chimney_y),(chimney_x + chimney_w, chimney_y),(chimney_x + chimney_w, chimney_y + chimney_h),(chimney_x, chimney_y + chimney_h)]
+    lineThickness = 3
+    pygame.draw.lines(screen, False, color, points, lineThickness)
+
+drawHouse(x,y,maja_w,maja_h,pind,red)
+drawDoor(x, y, maja_w, maja_h, pind, blue)
+drawWindow(x, y, maja_w, maja_h, pind, pink)
+drawChimney(x, y, maja_w, maja_h, pind, red)
 
 pygame.display.flip()
 
