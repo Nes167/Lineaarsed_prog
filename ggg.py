48df6371d074143a4f28dbb2f07669697168e605
DariaHalchenko <daragalcenko3@gmail.com>
2025-06-02 09:59:33 +0300

    ...

--- ggg.py
+++ ggg.py
@@ -63,17 +63,14 @@ def draw_text(text, x, y, color=white):
 
 while True:
     screen.blit(taust,(0,0))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:  
                 pygame.quit()
                 sys.exit()
-
             if not game_started and event.key == pygame.K_SPACE:
                 eggs = []
                 score = 0
@@ -82,7 +79,6 @@ while True:
                 game_over = False
                 game_started = True
                 pygame.mixer.music.play(-1)
-
             if game_over and event.key == pygame.K_r:
                 eggs = []
                 score = 0
@@ -91,20 +87,16 @@ while True:
                 game_over = False
                 game_started = True
                 pygame.mixer.music.play(-1)
-
         if event.type == spawn_event and game_started and not game_over:
             spawn_egg()
-
     keys = pygame.key.get_pressed()
     if game_started and not game_over:
         if keys[pygame.K_LEFT]:
             wolf_x -= wolf_speed
         if keys[pygame.K_RIGHT]:
             wolf_x += wolf_speed
-
         wolf_x = max(0, min(WIDTH - wolf_img.get_width(), wolf_x))
         screen.blit(wolf_img, (wolf_x, wolf_y))
-
         for egg in eggs[:]:
             egg['y'] += 5
             if egg['type'] == 'normal':
@@ -114,7 +106,6 @@ while True:
             elif egg['type'] == 'bomb':
                 img = bomb_img
             screen.blit(img, (egg['x'], egg['y']))
-
             if wolf_y < egg['y'] + img.get_height() < wolf_y + 20:
                 if wolf_x < egg['x'] < wolf_x + wolf_img.get_width():
                     if egg['type'] == 'normal':
@@ -127,29 +118,24 @@ while True:
                         bomb_sound.play()
                         lives -= 1
                     eggs.remove(egg)
-
             elif egg['y'] > HEIGHT:
                 if egg['type'] == 'normal':
                     bomb_sound.play()
                     lives -= 1
                 eggs.remove(egg)
-
             if lives <= 0:
                 game_over = True
                 pygame.mixer.music.stop()
                 game_over_sound.play()
         draw_text(f"Score: {score}", 10, 10)
         draw_text(f"Lives: {lives}", WIDTH - 150, 10)
-
     elif not game_started:
         draw_text("Alustamiseks vajutage SPACE", WIDTH // 2 - 150, HEIGHT // 2 - 100)
         draw_text("Väljumiseks vajutage ESC", WIDTH // 2 - 150, HEIGHT // 2 - 60)
-
     elif game_over:
         draw_text("Game over!", WIDTH // 2 - 100, HEIGHT // 2 - 50)
         draw_text(f"Score: {score}", WIDTH // 2 - 50, HEIGHT // 2)
         draw_text("Vajuta R, uue mängi alustamiseks", WIDTH // 2 - 180, HEIGHT // 2 + 50)
-
     pygame.display.flip()
     clock.tick(60)
 
